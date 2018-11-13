965c1ad770597c28f637ccb6baa770c49a9aabe3
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2018-11-13 18:15:14 +0100

    Adding product dialog

--- app/src/main/java/com/example/mariajoaomirapaulo/myshoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/mariajoaomirapaulo/myshoppinglist/ShoppingListActivity.java
@@ -1,10 +1,16 @@
 package com.example.mariajoaomirapaulo.myshoppinglist;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -30,19 +36,57 @@ public class ShoppingListActivity extends AppCompatActivity{
         addProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Add new Item");
+                openDialog();
             }
         });
 
     }
 
+    public void openDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New Product");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String productName = input.getText().toString();
+                ProductItem product = new ProductItem(productName);
+                addProduct(product);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        builder.show();
+    }
+
+    public void addProduct(ProductItem productItem){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "gestion", null, 1);
+        SQLiteDatabase database = admin.getWritableDatabase();
+
+        //add Product to database
+    }
+
     public void populateProductList(){
 
-        //testing list view
+        //testing list view To DELETE
+
         ProductItem bread = new ProductItem("bread");
         ProductItem book = new ProductItem("book");
         products.add(bread);
         products.add(book);
+
+
+        //get all products from database
 
         ProductAdapter productAdapter = new ProductAdapter(ShoppingListActivity.this, products);
         listProducts.setAdapter(productAdapter);
