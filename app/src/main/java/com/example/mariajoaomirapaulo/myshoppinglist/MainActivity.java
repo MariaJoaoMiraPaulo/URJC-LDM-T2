88163e5e76f9044244a715b62dc44146de47cdae
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2018-11-13 15:52:36 +0100

    Adding app icon. Adding menu.

--- app/src/main/java/com/example/mariajoaomirapaulo/myshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/mariajoaomirapaulo/myshoppinglist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mariajoaomirapaulo.myshoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shoppingList = (Button) findViewById(R.id.shopping_list);
+        Button manual = (Button) findViewById(R.id.manual);
+        Button historic = (Button) findViewById(R.id.historic);
+
+        shoppingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShoppingListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
